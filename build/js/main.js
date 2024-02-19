b4780e867fd0e349f0b383feb8d809ff2888a965
Milltiades <gigakakulia2017@gmail.com>
2024-02-19 19:38:43 +0400

    basic types

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,23 @@
 "use strict";
-let username = 'Giga';
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+// let username = 'Giga'
+// console.log(username)
+// let a: number = 12
+// let b: number = 6
+// let c: number = 2
+// console.log( a / b)
+// console.log(c * b)
+/* lesson 2 */
+let myName = 'Gedeon';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'john';
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
-let username = 'Giga'
-console.log(username)
+// let username = 'Giga'
+// console.log(username)
 
-let a: number = 12
-let b: number = 6
-let c: number = 2
+// let a: number = 12
+// let b: number = 6
+// let c: number = 2
 
-console.log( a / b)
+// console.log( a / b)
 
-console.log(c * b)
+// console.log(c * b)
+
+
+/* lesson 2 */
+
+let myName: string = 'Gedeon'
+let meaningOfLife: number
+let isLoading: boolean
+let album: any
+
+myName = 'john'
+meaningOfLife = 42
+isLoading = true
+album = 1984
+
+const sum = (a:string, b:number) => {
+    return a + b
+}
+
+let postId: string | number 
+let isActive: number | boolean 
+
+let re: RegExp = /\w+/g
